d41a594152a308bbb5da2a0d867f43bae7b2f49a
Jos Koot <jos.koot@gmail.com>
2025-03-06 03:11:03 +0100

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define scribblings (list (list "fmt.scrbl")))
-;; (define name "planet-fmt")
+(define name "fmt")
 (define blurb (list "A simple formatter"))
 (define collections "fmt")
-;; (define primary-file "fmt.rkt")
-;; (define categories (list 'io))
+(define primary-file "fmt.rkt")
+(define categories (list 'io))
 
