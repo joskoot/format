42bee5cabf2b73aed98d0fca2a697a2a883838a2
Jos Koot <jos.koot@gmail.com>
2025-03-13 00:42:53 +0100

    Add files via upload

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang setup/infotab
 (define scribblings (list (list "fmt.scrbl")))
-(define collection "fmt")
+
 (define blurb (list "A simple formatter"))
 (define primary-file "fmt.rkt")
 (define categories (list 'io))
